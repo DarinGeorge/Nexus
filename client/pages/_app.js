e9804ca7acdea0611efee737028f5d3bc598add3
Darin George <DarinGeorge>
2019-10-28 21:14:07 -0400

    fixed css mismatch bug, set up user post display

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import App from 'next/app';
+import Head from 'next/head';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import ApolloClient from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { createHttpLink } from 'apollo-link-http';
 import { ApolloProvider } from '@apollo/react-hooks';
 import fetch from 'isomorphic-unfetch';
+
+import { Layout } from '../components/_base/Layout';
 
 class MyApp extends App {
   // Only uncomment this method if you have blocking data requirements for
@@ -19,6 +23,14 @@ class MyApp extends App {
   //   return { ...appProps }
   // }
 
+  componentDidMount() {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentNode.removeChild(jssStyles);
+    }
+  }
+
   render() {
     const httpLink = createHttpLink({
       uri: 'http://localhost:5000',
@@ -31,9 +43,17 @@ class MyApp extends App {
     const { Component, pageProps } = this.props;
 
     return (
-      <ApolloProvider client={client}>
-        <Component {...pageProps} />
-      </ApolloProvider>
+      <>
+        <Head>
+          <title>Nexus</title>
+        </Head>
+        <ApolloProvider client={client}>
+          <CssBaseline />
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ApolloProvider>
+      </>
     );
   }
 }
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,16 +1,7 @@
 import React from 'react';
-import Head from 'next/head';
-import Nav from '../components/nav';
 
 const Home = () => (
   <div>
-    <Head>
-      <title>Home</title>
-      <link rel='icon' href='/favicon.ico' />
-    </Head>
-
-    <Nav />
-
     <div className='hero'>
       <h1 className='title'>Nexus</h1>
       <p className='description'>A Bleeding-Edge Social Network.</p>
